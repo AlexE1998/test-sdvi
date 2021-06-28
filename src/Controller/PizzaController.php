fe1d05bf62f8ba8890b284dd09d2ef950e1dcf94
AlexE1998 <aernault44@gmail.com>
2021-06-28 10:03:26 +0200

    New change

---
+++ src/Controller/PizzaController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use App\Service\Dao\PizzaDao;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PizzaController
+ * @package App\Controller
+ */
+class PizzaController extends AbstractController
+{
+    /**
+     * @Route("/pizzas")
+     *
+     * @param PizzaDao $pizzaDao
+     *
+     * @return Response
+     */
+    public function listeAction(PizzaDao $pizzaDao): Response
+    {
+        // récupération des différentes pizzas
+        $pizzas = $pizzaDao->getAllPizzas();
+
+        return $this->render("Pizza/liste.html.twig", [
+            "pizzas" => $pizzas,
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/pizzas/detail-{pizzaId}",
+     *     requirements={"pizzaId": "\d+"}
+     * )
+     *
+     * @param int $pizzaId
+     *
+     * @return Response
+     */
+    public function detailAction(int $pizzaId, PizzaDao $pizzaDao): Response
+    {
+        $pizza = $pizzaRepo->findPizzaAvecDetailComplet($pizzaId);
+        return $this->render("Pizza/detail.html.twig", ['pizza' => $pizza]);
+    }
+}
